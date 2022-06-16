93e210553d2b21e7a7f370b15f89c4b015172fc1
Anita Grigore <anita.grigore1@gmail.com>
2022-06-16 22:03:55 +0300

    Create themes for sidebar and button

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,73 @@ import { createRoot } from 'react-dom/client';
 import Button from '@mui/material/Button';
 
 import * as styles from './test.scss';
+import {Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ThemeProvider} from "@mui/material";
+import {drawerTheme} from "./themes/drawer";
+import HomeIcon from '@mui/icons-material/Home';
+import TaskIcon from '@mui/icons-material/Task';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import GroupsIcon from '@mui/icons-material/Groups';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {buttonTheme} from "./themes/button";
 
 function Counter() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <h1 className={styles.counter}>{count}</h1>
-      <Button onClick={() => setCount(count + 1)}>
-        Increment
-      </Button>
+      <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <ThemeProvider theme={drawerTheme}>
+            <Drawer variant="permanent" anchor="left">
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <HomeIcon sx={{color: '#DDDDDD'}} />
+                    </ListItemIcon>
+                    <ListItemText primary = "Dashboard" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <TaskIcon sx={{color: '#DDDDDD'}} />
+                    </ListItemIcon>
+                    <ListItemText primary = "Your Courses" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <ManageAccountsIcon sx={{color: '#DDDDDD'}} />
+                    </ListItemIcon>
+                    <ListItemText primary = "Accounts" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <GroupsIcon sx={{color: '#DDDDDD'}} />
+                    </ListItemIcon>
+                    <ListItemText primary = "Groups" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <SettingsIcon sx={{color: '#DDDDDD'}} />
+                    </ListItemIcon>
+                    <ListItemText primary = "Preferences" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Drawer>
+          </ThemeProvider>
+        </Grid>
+        <Grid item xs={10}>
+          <ThemeProvider theme={buttonTheme}>
+            <Button variant='text'>Say Hello</Button>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
     </>
   );
 }
