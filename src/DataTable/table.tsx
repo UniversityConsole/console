81a4ffdc73a05dae1667b35a2d44aa98730ee558
Anita Grigore <anita.grigore1@gmail.com>
2022-06-18 20:21:38 +0300

    Generalize table component

---
+++ src/DataTable/table.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@mui/material';
+import TablePaginationActions from "./pagination";
+
+interface Props<T> {
+  readonly columnDefinitions: ColumnDefinition<T>[];
+  readonly isLoading: boolean;
+  readonly items: T[];
+}
+
+interface ColumnDefinition<T> {
+  readonly id: string;
+  readonly head: React.ReactNode;
+  readonly cell: (item: T) => React.ReactNode;
+}
+
+export default function PaginatedTable<T>(props: Props<T>) {
+  const {items, columnDefinitions, isLoading} = props;
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - items.length) : 0;
+
+  const pageItems = rowsPerPage > 0
+    ? items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    : items;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table stickyHeader sx={{minWidth: 500}}>
+        <TableHead>
+          <TableRow>
+            {columnDefinitions.map(c => (
+              <TableCell key={c.id}>{c.head}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {!isLoading && pageItems.map((item, idx) => (
+            <TableRow key={idx}>
+              {columnDefinitions.map(column => (
+                <TableCell key={`${idx}-${column.id}`}>
+                  {column.cell(item)}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+          {!isLoading && emptyRows > 0 && (
+            <TableRow style={{height: 53 * emptyRows}}>
+              <TableCell colSpan={columnDefinitions.length}/>
+            </TableRow>
+          )}
+          {isLoading && <TableRow>
+              <TableCell colSpan={columnDefinitions.length}>Loading...</TableCell>
+          </TableRow>}
+        </TableBody>
+        {!isLoading && <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+              colSpan={columnDefinitions.length}
+              count={items.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>}
+      </Table>
+    </TableContainer>
+  );
+}
--- src/accounts.tsx
+++ src/accounts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import CustomPaginationActionsTable from "./paginatedTable";
 import {ConsoleHeader} from "./ConsoleHeader";
 import {Button} from "@mui/material";
+import PaginatedTable from "./DataTable/table";
 
 export default function Accounts() {
   const consoleActions = (
@@ -10,12 +10,30 @@ export default function Accounts() {
     </>
   );
 
+  const accounts = [
+    { id: '123', firstName: 'Victor', lastName: 'Barbu', email: 'vicbarbu@pm.me' },
+    { id: '234', firstName: 'Anita', lastName: 'Grigore', email: 'anita.grigore1@gmail.com' },
+    { id: '345', firstName: 'John', lastName: 'Doe', email: 'john@example.com' },
+    { id: '2345', firstName: 'Jane', lastName: 'Doe', email: 'jane@example.com' },
+  ];
+
   return (
     <>
       <ConsoleHeader title="Accounts" actions={consoleActions}>
         View and manage the user accounts registered on University Console.
       </ConsoleHeader>
-      <CustomPaginationActionsTable/>
+      <PaginatedTable
+        columnDefinitions={[
+          { id: 'id', head: 'ID', cell: item => item.id },
+          { id: 'firstName', head: 'First Name', cell: item => item.firstName },
+          { id: 'lastName', head: 'Last Name', cell: item => item.lastName },
+          { id: 'actions', head: '', cell: () => (
+            <Button variant="text">Contact Details</Button>
+            ) },
+        ]}
+        items={accounts}
+        isLoading={false}
+      />
     </>
   );
 }
